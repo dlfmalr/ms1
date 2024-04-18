d5aa3a93f04884ac8c6f89962b91ba1e7ace50f4
dlfmalr <sonhyeseung05@gmail.com>
2024-04-18 15:17:00 +0900

    1-0

---
+++ src/main/java/com/example/ms1/NoteController.java
@@ -0,0 +1,63 @@
+package com.example.ms1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class NoteController {
+
+    private final NoteRepository noteRepository;
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(Model model) {
+        return "test";
+    }
+
+    @RequestMapping("/")
+    public String main(Model model) {
+        List<Note> noteList = noteRepository.findAll();
+
+        model.addAttribute("noteList", noteList);
+        model.addAttribute("targetNote", noteList.get(0));
+
+        return "main";
+    }
+
+    @PostMapping("/write")
+    public String write() {
+        Note note = new Note();
+        note.setTitle("new title");
+        note.setContent("");
+        note.setCreateDate(LocalDateTime.now());
+
+        noteRepository.save(note);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable Long id) {
+        Note note = noteRepository.findById(id).get();
+        model.addAttribute("targetNote", note);
+        model.addAttribute("noteList", noteRepository.findAll());
+
+        return "main";
+    }
+
+    @PostMapping("/update")
+    public String update(Long id, String title, String content) {
+        Note note = noteRepository.findById(id).get();
+        note.setTitle(title);
+        note.setContent(content);
+
+        noteRepository.save(note);
+        return String.format("redirect:/detail/%s" + id);
+    }
+}
