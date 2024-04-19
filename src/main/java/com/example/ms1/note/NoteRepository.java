199cd20ec059fdb25340d194587c4b5860d8ed3e
dlfmalr <sonhyeseung05@gmail.com>
2024-04-19 10:08:43 +0900

    1-8

--- src/main/java/com/example/ms1/note/NoteRepository.java
+++ src/main/java/com/example/ms1/note/NoteRepository.java
@@ -1,6 +1,5 @@
 package com.example.ms1.note;
 
-import com.example.ms1.note.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
